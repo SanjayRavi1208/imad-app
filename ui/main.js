3d46037eca058611e5d40e972279ede5719a6e39
SanjayRavi1208 <30874952+SanjayRavi1208@users.noreply.github.com>
2017-08-20 23:21:21 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -6,7 +6,7 @@ var img=document.getElementById('sansa');
 var marginLeft=0;
 function moveRight()
 {
-    marginLeft=marginLeft+1;
+    marginLeft=marginLeft+10;
     sansa.style.marginLeft=marginLeft+'px';
 }
 sansa.onclick=function()
