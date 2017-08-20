9e5766323662d01c293ec1cf0043ceacae70ba32
SanjayRavi1208 <30874952+SanjayRavi1208@users.noreply.github.com>
2017-08-20 23:09:47 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,7 +3,14 @@ var element=document.getElementById('main.text');
 element.innerHTML='newvalue';
 //Moving Image
 var img=document.getElementById('sansa');
+var marginLeft=0;
+function moveRight()
+{
+    marginLeft=marginLeft+100;
+    sansa.style.marginLeft=marginLeft+'px';
+}
 sansa.onclick=function()
 {
+    var interval=setInterval(moveRight,100);
     img.style.marginLeft='100px';
 }
