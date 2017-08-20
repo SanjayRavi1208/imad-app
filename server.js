e9d89828c0b00f3f023b056bb439ddb6984dd27c
SanjayRavi1208 <30874952+SanjayRavi1208@users.noreply.github.com>
2017-08-21 01:15:50 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -89,6 +89,12 @@ app.get('/', function (req, res) {
 app.get('/ui/main.js', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'main.js'));
 });
+var counter=0;
+app.get('/counter',function(req,res)
+{
+    counter=counter+1;
+    res.send(counter.toString());
+});
 app.get('/:articleName',function(req,res)
 {
     var articleName=req.params.articleName;
