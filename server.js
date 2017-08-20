4815885ad722df4b776a46c7bc3ce0a742521e7b
SanjayRavi1208 <30874952+SanjayRavi1208@users.noreply.github.com>
2017-08-20 15:09:42 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -8,7 +8,18 @@ app.use(morgan('combined'));
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
-
+app.get('/article-one',function(req,res)
+{
+    res.send('Article one Requested');
+});
+app.get('/article-one',function(req,res)
+{
+    res.send('Article one Requested');
+});
+app.get('/article-one',function(req,res)
+{
+    res.send('Article one Requested');
+});
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
