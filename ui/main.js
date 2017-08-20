2d92c174c07d69cedacde6ccc0053bf0dabcb46e
SanjayRavi1208 <30874952+SanjayRavi1208@users.noreply.github.com>
2017-08-20 23:04:20 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,3 +1,9 @@
 console.log('Loaded!');
 var element=document.getElementById('main.text');
 element.innerHTML='newvalue';
+//Moving Image
+var img=document.getElementById('sansa');
+sansa.onclick=function()
+{
+    img.style.marginLeft='100px';
+}
