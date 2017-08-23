ae6fca855091e318a57867230fe85481b9d50f8e
SanjayRavi1208 <30874952+SanjayRavi1208@users.noreply.github.com>
2017-08-24 01:40:38 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -135,15 +135,15 @@ app.get('/articles/:articleName',function(req,res)
         }
         else
         {
-            if(result.rows.length===0)
-            {
+          //  if(result.rows.length===0)
+          //  {
                 res.status(404).send('Article Not found');
-            }
-            else
-            {
-                var articleData=result.rows[0];
-                    res.send(createTemplate(articledata));
-            }
+          //  }
+          //  else
+          //  {
+          //      var articleData=result.rows[0];
+         //           res.send(createTemplate(articledata));
+         //   }
         }
     });
 
