7e848032b96fbc377893fa76442fc182c9bc8f91
SanjayRavi1208 <30874952+SanjayRavi1208@users.noreply.github.com>
2017-08-25 21:18:28 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -98,7 +98,7 @@ app.get('/articles/:articleName',function(req,res)
             else
             {
                      var articleData=result.rows[0];
-                    res.send(createTemplate(articleData));
+                 //   res.send(createTemplate(articleData));
                     res.send(JSON.stringify(rows.length));
                  //   res.send(JSON.stringify(result.rows));
             }
