7a6a185a5c8e5e3379128e31b4942f50ea4391c6
SanjayRavi1208 <30874952+SanjayRavi1208@users.noreply.github.com>
2017-08-25 22:47:22 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -24,7 +24,7 @@ button.onclick=function()
                    var counter=request.responseText;
                    counter=JSON.parse(counter);
                    var count1=counter[0];
-                   console.log(count1);
+                   console.log(count1['count']);
                    var span=document.getElementById('count');
                    span.innerHTML=count;
         }
