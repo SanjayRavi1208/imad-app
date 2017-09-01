108142f629f0c968e53b4c45124927509dd83c41
SanjayRavi1208 <30874952+SanjayRavi1208@users.noreply.github.com>
2017-09-01 16:27:57 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
 var Pool=require('pg').Pool;
+var crypto=require('crypto');
 var config={
   user:'sanjaykr1208',
   databse:'sanjaykr1208',
@@ -49,6 +50,15 @@ function createTemplate(data)
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
   console.log('man');
+});
+function hash (input,salt)
+{
+    var hashed=crypto.pbkdf2Sync(input,salt,10000,512,'sha512');
+    return hashed;
+}
+app.get('/hash/:input',function(req,res){
+    var hashedString=hash(req.params.input,'this-is-some-random-string');
+    res.send(hashedString);
 });
 var pool=new Pool(config);
 
@@ -103,6 +113,7 @@ app.get('/sub-name',function(req,res)
     res.send(JSON.stringify(names));
     
 });
+
 app.get('/articles/:articleName',function(req,res)
 {
     //var articledata=
