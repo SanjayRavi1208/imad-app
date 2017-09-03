712131ca95a72cb0ed43a899a8d98b0ed222581a
SanjayRavi1208 <30874952+SanjayRavi1208@users.noreply.github.com>
2017-09-03 23:50:39 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -28,3 +28,31 @@ register.onclick=function()
     log.setRequestHeader('Content-Type','application/json');
     log.send(JSON.stringify({username:username,password:password}));
 };
+var login=document.getElementById('log');
+login.onclick=function()
+{
+    var log=new XMLHttpRequest();
+    log.onreadystatechange=function()
+    {
+        if(log.readyState===XMLHttpRequest.DONE)
+        {
+            if(log.status===200)
+            {
+                alert('Login Sucessful ');
+            }   
+            else if(log.status===403)
+            {
+                alert('Invalid User');
+            }
+            else
+            {
+                alert('Unknown error Occured!!!Try Again...');
+            }
+        }
+    };
+    var username=document.getElementById('username').value;
+    var password=document.getElementById('pass').value;
+    log.open('POST','http://sanjaykr1208.imad.hasura-app.io/login',true);
+    log.setRequestHeader('Content-Type','application/json');
+    log.send(JSON.stringify({username:username,password:password}));
+};
